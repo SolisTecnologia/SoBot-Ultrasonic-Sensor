d21c21135839327783c252b44149e50aa2b03790
SolisTecnologia <solistecnologia2627@gmail.com>
2021-04-30 14:48:03 -0300

    Initial commit

---
+++ check_sonar.py
@@ -0,0 +1,94 @@
+from time import sleep
+import serial
+
+data_sonar = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+Zero = 48
+Um = 49
+Dois = 50
+Tres = 51
+Quatro = 52
+Cinco = 53
+Seis = 54
+Sete = 55
+Oito = 56
+Nove = 57
+
+# Set serial port
+usb = serial.Serial('/dev/ttyACM0', 57600, timeout=0, dsrdtr=False)
+usb.flush()     # Waits data configuration
+
+def sonar_value():
+    value = [0,0,0,0,0,0,0,0]
+    usb.write(b"SS0")            # Send command to read sonar sensor
+    sleep(0.5)                   # Wait to return datas
+    data_sonar = usb.readline()  # Read data
+    print(data_sonar)
+    # Check front sensor if it is <= 50cm
+    if((data_sonar[13] == Zero) and (((data_sonar[14] == Cinco) and (data_sonar[15] <= Zero)) or (data_sonar[14] <= Quatro))):
+        value[1] = 1
+    else:
+        value[1] = 0
+    # Check left front sensor if it is <= 45cm
+    if((data_sonar[4] == Zero) and (((data_sonar[5] == Quatro) and (data_sonar[6] <= Cinco)) or (data_sonar[5] <= Tres))):
+        value[0] = 1
+    else:
+        value[0] = 0
+    # Check right front sensor if it is <= 45cm
+    if((data_sonar[22] == Zero) and (((data_sonar[23] == Quatro) and (data_sonar[24] <= Cinco)) or (data_sonar[23] <= Tres))):
+        value[2] = 1
+    else:
+        value[2] = 0
+    # Check which side is farthest from obstacle
+    if(data_sonar[31] == data_sonar[67]):
+        if(data_sonar[32] == data_sonar[68]):
+            if(data_sonar[33] == data_sonar[69]):
+                # Check Right sensor is greater
+                if(data_sonar[34] > data_sonar[70]):
+                    value[3] = 0
+                    value[7] = 1
+                else:
+                    value[3] = 1
+                    value[7] = 0
+            else:
+                # Check Right sensor is greater
+                if(data_sonar[33] > data_sonar[69]):
+                    value[3] = 0
+                    value[7] = 1
+                else:
+                    value[3] = 1
+                    value[7] = 0
+        else:
+            # Check Right sensor is greater
+            if(data_sonar[32] > data_sonar[68]):
+                value[3] = 0
+                value[7] = 1
+            else:
+                value[3] = 1
+                value[7] = 0
+    else:
+        # Check Right sensor is greater
+        if(data_sonar[31] > data_sonar[67]):
+            value[3] = 0
+            value[7] = 1
+        else:
+            value[3] = 1
+            value[7] = 0
+    # Check right back sensor if it is <= 15cm
+    if((data_sonar[40] == Zero) and (((data_sonar[41] == Um) and (data_sonar[42] <= Cinco)) or (data_sonar[41] == Zero))):
+        value[4] = 1
+    else:
+        value[4] = 0
+
+    # Check back sensor if it is <= 15cm
+    if((data_sonar[49] == Zero) and (((data_sonar[50] == Um) and (data_sonar[51] <= Cinco)) or (data_sonar[50] == Zero))):
+        value[5] = 1
+    else:
+        value[5] = 0
+    # Check left back sensor if it is <= 15cm
+    if((data_sonar[58] == Zero) and (((data_sonar[59] == Um) and (data_sonar[60] <= Cinco)) or (data_sonar[59] == Zero))):
+        value[6] = 1
+    else:
+        value[6] = 0
+
+    return value
---
+++ sonar_sensor.py
@@ -0,0 +1,164 @@
+from time import sleep
+import serial
+import check_sonar
+
+sonar_sensor = [0,0,0,0,0,0,0,0]
+flag_enable = 0
+flag_mv_fw = 0
+count_mv_bk = 0
+count_mv_lf = 0
+count_mv_rd = 0
+
+# Set serial port
+usb = serial.Serial('/dev/ttyACM0', 57600, timeout=0, dsrdtr=False)
+usb.flush()         # Waits data configuration
+
+usb.write(b"LT E1 RD0 GR50 BL0")    # Turn on led tape in green
+sleep(1)
+usb.write(b"MT0 MC AT100 DT100 V2") # Parameter settings for continuous mode
+sleep(1)
+usb.write(b"MT0 ME1")               # Enables wheel motors on mode continuous
+sleep(1)
+
+while(flag_enable == 0):
+    sonar_sensor = check_sonar.sonar_value()
+    print(sonar_sensor)
+    # Check front sensors
+    if((sonar_sensor[0] == 0) and (sonar_sensor[1] == 0) and (sonar_sensor[2] == 0)):
+        if(flag_mv_fw == 0):
+            flag_fw = 1
+            usb.write(b"LT E1 RD0 GR50 BL0")
+            usb.write(b"MT0 MF")        # Moving to forward
+    # Check the front sensor only
+    elif(sonar_sensor[1] == 1):
+        # Check the right side sensor is smaller
+        if((sonar_sensor[3] == 1) and (sonar_sensor[7] == 0)):
+            # Check the right rear side sensor
+            if(sonar_sensor[4] == 0):
+                usb.write(b"LT E1 RD0 GR0 BL50")
+                usb.write(b"MT0 ML")        # Moviment to left
+            # Check the left rear side sensor
+            elif(sonar_sensor[6] == 0):
+                count_mv_rd = 0
+                while(count_mv_rd < 12):
+                    count_mv_rd+= 1
+                    sonar_sensor = check_sonar.sonar_value()
+                    if(sonar_sensor[6] == 0):
+                        usb.write(b"LT E1 RD0 GR30 BL10")
+                        usb.write(b"MT0 MR")        # Moviment to right
+                    else:
+                        count_mv_rd = 12
+                count_mv_bk = 0
+                usb.write(b"LT E1 RD50 GR0 BL0")
+                while(count_mv_bk < 6):
+                    count_mv_bk += 1
+                    sonar_sensor = check_sonar.sonar_value()
+                    # Checks the rear sensors
+                    if((sonar_sensor[4] == 0) and (sonar_sensor[5] == 0) and (sonar_sensor[6] == 0)):
+                        usb.write(b"MT0 MB")        # Moving to back
+                    else:
+                        count_mv_bk = 6
+            # If the two rear sides sensors are locked
+            else:
+                usb.write(b"MT0 MP")        # Moviment pause
+                flag_enable = 1
+        # Check the left side sensor is smaller
+        elif((sonar_sensor[3] == 0) and (sonar_sensor[7] == 1)):
+            # Check the left rear side sensor
+            if(sonar_sensor[6] == 0):
+                usb.write(b"LT E1 RD0 GR30 BL10")
+                usb.write(b"MT0 MR")        # Moviment to right
+            # Check the right rear side sensor
+            elif(sonar_sensor[4] == 0):
+                count_mv_lf = 0
+                while(count_mv_lf < 12):
+                    count_mv_lf+= 1
+                    sonar_sensor = check_sonar.sonar_value()
+                    if(sonar_sensor[4] == 0):
+                        usb.write(b"LT E1 RD0 GR0 BL50")
+                        usb.write(b"MT0 ML")        # Moviment to left
+                    else:
+                        count_mv_lf = 12
+                count_mv_bk = 0
+                usb.write(b"LT E1 RD50 GR0 BL0")
+                while(count_mv_bk < 6):
+                    count_mv_bk += 1
+                    sonar_sensor = check_sonar.sonar_value()
+                    # Checks the rear sensors
+                    if((sonar_sensor[4] == 0) and (sonar_sensor[5] == 0) and (sonar_sensor[6] == 0)):
+                        usb.write(b"MT0 MB")        # Moving to back
+                    else:
+                        count_mv_bk = 6
+            # If the two rear sides sensors are locked
+            else:
+                usb.write(b"MT0 MP")        # Moviment pause
+                flag_enable = 1
+    # Check the left front sensor only
+    elif(sonar_sensor[0] == 1):
+        # Check the left rear side sensor
+        if(sonar_sensor[6] == 0):
+            usb.write(b"LT E1 RD0 GR30 BL10")
+            usb.write(b"MT0 MR")        # Moviment to right
+        # Check the right rear side sensor
+        elif(sonar_sensor[4] == 0):
+            count_mv_lf = 0
+            while(count_mv_lf < 12):
+                count_mv_lf+= 1
+                sonar_sensor = check_sonar.sonar_value()
+                if(sonar_sensor[4] == 0):
+                    usb.write(b"LT E1 RD0 GR0 BL50")
+                    usb.write(b"MT0 ML")    # Moviment to left
+                else:
+                    count_mv_lf = 12
+            count_mv_bk = 0
+            usb.write(b"LT E1 RD50 GR0 BL0")
+            while(count_mv_bk < 6):
+                count_mv_bk += 1
+                sonar_sensor = check_sonar.sonar_value()
+                # Checks the rear sensors
+                if((sonar_sensor[4] == 0) and (sonar_sensor[5] == 0) and (sonar_sensor[6] == 0)):
+                    usb.write(b"MT0 MB")        # Moving to back
+                else:
+                    count_mv_bk = 6
+        # If the two rear sides sensors are locked
+        else:
+            usb.write(b"MT0 MP")        # Moviment pause
+            flag_enable = 1
+    # Check the right front sensor only
+    elif(sonar_sensor[2] == 1):
+        # Check the right rear side sensor
+        if(sonar_sensor[4] == 0):
+            usb.write(b"LT E1 RD0 GR0 BL50")
+            usb.write(b"MT0 ML")        # Moviment to left
+        # Check the left rear side sensor
+        elif(sonar_sensor[6] == 0):
+            count_mv_rd = 0
+            while(count_mv_rd < 12):
+                count_mv_rd+= 1
+                sonar_sensor = check_sonar.sonar_value()
+                if(sonar_sensor[6] == 0):
+                    usb.write(b"LT E1 RD0 GR30 BL10")
+                    usb.write(b"MT0 MR")        # Moviment to right
+                else:
+                    count_mv_rd = 12
+            count_mv_bk = 0
+            usb.write(b"LT E1 RD50 GR0 BL0")
+            while(count_mv_bk < 6):
+                count_mv_bk += 1
+                sonar_sensor = check_sonar.sonar_value()
+                # Checks the rear sensors
+                if((sonar_sensor[4] == 0) and (sonar_sensor[5] == 0) and (sonar_sensor[6] == 0)):
+                    usb.write(b"MT0 MB")        # Moving to back
+                else:
+                    count_mv_bk = 6
+        # If the two rear sides sensors are locked
+        else:
+            usb.write(b"MT0 MP")        # Moviment pause
+            flag_enable = 1
+
+usb.write(b"MT0 MP")        # Moviment pause
+usb.write(b"LT E1 RD50 GR30 BL0")
+sleep(4)
+
+usb.write(b"LT E0")
+usb.write(b"MT0 ME0")       # Disables wheel motors on mode continuous
